fa4dc7e7ccea07785e742ce600ff000acad2274b
Alex <alex@yang2k.com>
2019-11-04 20:57:28 -0600

    Updates

---
+++ precache-manifest.da9fd103ef8e6a80b6df6ddc8433b67f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f34307fbe46a3ddf2a83bc5b6f9ac63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64da9834e096bca31c54",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "aac545b62f840190b4fa",
+    "url": "/static/js/2.cf1cbabf.chunk.js"
+  },
+  {
+    "revision": "64da9834e096bca31c54",
+    "url": "/static/js/main.53de0589.chunk.js"
+  },
+  {
+    "revision": "ef2dcd888cb95d19f615",
+    "url": "/static/js/runtime-main.f501695b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
