3fad823fdbc98a822e1658d325bf9bc25409d964
Alex <alex@yang2k.com>
2019-11-04 23:27:05 -0600

    Formatting

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React from 'react';
 //import niagra from './niagra.jpg';
 import './App.css';
 
+const Banner = (props) => {
+    return(
+        <>
+            <div class="banner">
+                Alex Yang
+            </div>
+        </>
+    );
+}
+
+let images = ["myself", "Hangouts", "scio", "robotics"];
+
 class Name extends React.Component {
   constructor(props) {super(props);}
 
@@ -14,14 +26,14 @@ class Name extends React.Component {
   }
 }
 
-class Left extends React.Component {
+class Text extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
     return (
-        <div class="left">
+        <div class="text">
           <header class="main-header">
             I'm <Name name="Alex Yang"/>
           </header>
@@ -30,27 +42,54 @@ class Left extends React.Component {
   }
 }
 
-class Right extends React.Component {
+class Images extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return (
-        <div class="right">
-            <div class="pseudo-img">Myself</div>
+      let imageCards = images.map(imageName => {
+          return(
+              <div className="pseudo-img" id={imageName}>
+                  {imageName}
+              </div>
+          )
+      });
+
+      return (
+        <div class="images">
+            {imageCards}
         </div>
     )
   }
 }
 
+// function ScrollButton = (props) => {
+//     return(
+//       <a href="#section02"></a>
+//     );
+// }
+
+const Footer = (props) => {
+    return(
+      <footer>
+        <h1>Let's Get In Touch!</h1>
+          <div class="contact">Contact me!</div>
+      </footer>
+    );
+}
 
 function App() {
   return (
-    <main>
-      <Left/>
-      <Right/>
-    </main>
+    <>
+        <Banner/>
+        <main>
+            <Images/>
+            <Text/>
+            {/*<ScrollButton/>*/}
+        </main>
+        <Footer/>
+    </>
   );
 }
 
