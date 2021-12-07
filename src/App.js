0d737692503d95fae030a64d1786b33d0b4bf667
theuncleofAlex <alex@yang2k.com>
2021-12-06 20:57:14 -0500

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.png';
-import arrow from './Arrow.png';
+import arrow from './arrow.png';
 import './App.css';
 import { useRef, useLayoutEffect } from 'react';
 
@@ -81,7 +81,7 @@ const images = [
     }, {
         id: "hangouts",
         link: "Hobbies",
-        description: ["Github ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."],
+        description: ["Github ipsum ddolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."],
         cardHeader: "See my projects",
         cards: [
             {
